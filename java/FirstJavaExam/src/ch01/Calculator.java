ffbd1970ccbd61a49ef1a270e908caa91dcbb9e6
kimrumm <kimrumm@naver.com>
2021-05-26 23:34:06 +0900

    ch01
    ch01 Calculator 클래스를 정의해 봅시다.

---
+++ java/FirstJavaExam/src/ch01/Calculator.java
@@ -0,0 +1,37 @@
+package ch01;
+
+public class Calculator {
+
+//		1.숫자 두 개를 매개변수의 인자로 전달받아 더하기 메소드를 추가합시다. 
+		long plus (long a, long b) {
+//			long result = a + b;
+//			return result;
+			return a + b;
+		}
+//		2.숫자 두 개를 매개변수의 인자로 전달받아 빼기 메소드를 추가합시다. 
+		long minus (long a, long b) {
+//			int result = a - b;
+//			return result;
+			return a - b;
+		}
+		
+//		3.숫자 두 개를 매개변수의 인자로 전달받아 곱하기 메소드를 추가합시다. 
+		long multi (long a, long b) {
+//			int result = a * b;
+//			return result;
+			return a * b;
+		}
+		
+//		4.숫자 두 개를 매개변수의 인자로 전달받아 나누기 메소드를 추가합시다.
+		double div (double a, double b) {
+//			double result = a / b;
+//			return result;
+			return a * b;
+		}
+		
+	public static void main(String[] args) {
+		
+
+	}
+
+}
