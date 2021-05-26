d2d08c13ca4b4a5b93d4338a00de2aaff938fb36
kimrumm <kimrumm@naver.com>
2021-05-26 21:42:15 +0900

    ch01
    ch01 문제

---
+++ java/FirstJavaExam/src/ch01/Member.java
@@ -0,0 +1,13 @@
+package ch01;
+	 	// Member 클래스를 생성하고, 
+public class Member {
+
+	public static void main(String[] args) {
+		
+		
+		// main()메소드를 만들고 자기 자신의 이름을 출력하는 메소드를 만들어 출력해 봅시다. 
+		System.out.println("안녕하세요. 저는 김아름 입니다.");
+
+	}
+
+}
