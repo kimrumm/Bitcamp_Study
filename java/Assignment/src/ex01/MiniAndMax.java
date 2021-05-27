a55c69207d3a3816246b0bd6473165b56e3cd102
kimrumm <83346234+kimrumm@users.noreply.github.com>
2021-05-27 18:23:58 +0900

    ArrayEx01
    최소값과 최대값 사용자에게 입력받아서 구하기

--- java/Assignment/src/ex01/MiniAndMax.java
+++ java/Assignment/src/ex01/MiniAndMax.java
@@ -1,43 +1,46 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class MiniAndMax {
-//		문제 1.
-//		int형 1차원 배열을 매개변수로 전달 받아서 배열에 저장된 최대값,
-//		그리고 최소값을 구해서 반환하는 메소드를 다음의 형태로 각각 정의.
-	static int arr[] = {1,5,7,10,62,4};
-	static int miniValue = arr[0];
-	static int maxValue = arr[0];
-	
-	
+
 // 최소값 반환
-	public static int miniValue(int[] arr) { 
-		for(int i=0; i < arr.length; i++) {
-			if(arr[i] < miniValue) {
-				miniValue = arr[0];
-				
+	public static int miniValue(int[] arr) {
+		int min = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+
 			}
-			
+
 		}
-		return miniValue;
-	} 
-	public static int maxValue(int[] arr) { 
-		
-	} // 최대값 반환
-	
-//		위의 두 매소드는 인자로 전달되는 배열의 길이에 상관없이 동작하도록 정의.
+		return min;
+	}
+
+// 최대값 반환
+	public static int maxValue(int[] arr) {
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
 
 	public static void main(String[] args) {
-		MiniAndMax mam = new MiniAndMax(); 
-		
-		System.out.println();
-		
-		
-		
-//		두 매소드 실행을 확인인하기 위한 main 메소드 정의.
-//		int형 배열에 채워질 정수는 프로그램 사용자로부터 입력 받고, 배열의 길이는 임의로 결정
+		MiniAndMax arr = new MiniAndMax();
 
-		​
-		
+		// 사용자로 부터 입력받기
+		int[] arrInt = new int[5];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수를 5개를 입력하세요 : ");
+		for (int i = 0; i < arrInt.length; i++) {
+			arrInt[i] = sc.nextInt();
+		}
+
+		System.out.println("최소값 : " + miniValue(arrInt));
+		System.out.println("최대값 : " + maxValue(arrInt));
 
 	}
 
