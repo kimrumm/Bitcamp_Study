d0b9941bd6258b0416055498cab555f4fbcd9af8
kimrumm <83346234+kimrumm@users.noreply.github.com>
2021-05-27 14:33:38 +0900

    ch02
    ch02 문제

---
+++ java/FirstJavaExam/src/ch02/Member.java
@@ -0,0 +1,27 @@
+package ch02;
+
+public class Member {
+
+	public static void main(String[] args) {
+		
+//		1. String 타입의 이름을 저장할 수 있는 변수 name을 정의해봅시다.
+		String name = "김아름";
+		
+//		2. int 타입의 나이를 저장할 수 있는 변수 age를 정의해봅시다.
+		int age = 28;
+		
+//		3. double 타입의 키를 저장할 수 있는 변수 height를 정의해봅시다.
+		double height = 158;
+		
+//		4. boolean 타입의 JAVA책의 보유 여부를 저장할 수 있는 변수 hasBook를 정의해봅시다. 
+		boolean hasBook = false;
+		
+//		5. 이름과 나이, 키, 책의 보유 여부를 출력해봅시다.
+		System.out.println("이름: " + name + " 나이: " +" 키: " + height 
+							+ " 책이 있습니까? " + hasBook);
+
+		
+
+	}
+
+}
