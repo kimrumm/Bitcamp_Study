f2cce6c6b587b4be7fe34cf352a572b6aca5e379
kimrumm <kimrumm@naver.com>
2021-06-17 01:55:46 +0900

    21.06.16

--- SQL/SQL_EXAMPLE/join_exam.sql
+++ SQL/SQL_EXAMPLE/join_exam.sql
@@ -80,20 +80,42 @@ order by e.ename;
 --39. OUTER JOIN, SELF JOIN을 사용하여 
 -- 관리자가 없는 사원을 포함하여 
 -- 사원번호를 기준으로 내림차순 정렬하여 출력하시오.
+select e.ename,e.empno,m.ename
+from emp e, emp m
+where e.mgr=m.empno(+)
+order by e.empno desc ;
 
 
 --40. SELF JOIN을 사용하여 
 -- 지정한 사원의 이름, 부서번호, 지정한 사원과 
 -- 동일한 부서에서 근무하는 사원을 출력하시오. ( SCOTT )
+select ename from emp where deptno=20;
+
+select e.ename, e.empno, s.ename as sname
+from emp e, emp s
+where e.ename='SCOTT'
+and e.deptno = s.deptno
+and s.ename != 'SCOTT'
+;
+
 
 
 --41. SELF JOIN을 사용하여 
 -- WARD 사원보다 늦게 입사한 
 -- 사원의 이름과 입사일을 출력하시오.
-
+select *
+from emp e, emp w
+where w.ename = 'WARD'
+and e.hiredate > w.hiredate
+order by e.hiredate
+;
 
 
 --42. SELF JOIN 을 사용하여 
 -- 관리자보다 먼저 입사한 모든 사원의 이름 및 입사일을 
 -- 관리자의 이름 및 입사일과 함께 출력하시오.
+select e.ename, e.hiredate, m.ename, m.hiredate
+from emp e, emp m
+where e.mgr = m.empno
+and e.hiredate < m.hiredate;
 
