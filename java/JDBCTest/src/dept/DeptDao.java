6817fe69c5f84278e8fddacc141a75cb6194a8e9
Kim Arum <kimrumm@naver.com>
2021-06-18 18:24:22 +0900

    JDBC

--- java/JDBCTest/src/dept/DeptDao.java
+++ java/JDBCTest/src/dept/DeptDao.java
@@ -8,23 +8,27 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DeptDao {
-
-	// 싱글톤 패턴 : 여러개의 인스턴스를 생성하지 못하도록 하는 디자인 패턴(코딩 스타일)
-
-	// 1. 외부 클래스 또는 인스턴스에서 해당 클래스로 인슨턴스를 생성하지 못하도록 처리
-
-	private DeptDao() {
-
+	
+	// 싱글톤 패턴 : 여러개의 인스턴스를 생 성하지 못하도록 하는 디자인 패턴
+	// 1. 외부 클래스 또는 인스턴스에서 해당 클래스로 인스턴스를 생성하지 못하도록 처리
+	private DeptDao(){
 	}
-
-	// 2. 클래스 내부에서 인스턴스를 만들고
-	static private  DeptDao dao = new DeptDao();
-
+	
+	// 2. 클래스 내부에서 인스턴스를 만들고 
+	static private DeptDao dao = new DeptDao(); 
+	
 	// 3. 메소드를 통해서 반환 하도록 처리
-	public static DeptDao getInstanace() {
+	public static DeptDao getInstance() {
 		return dao;
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
 	// 1. 전체 데이터 검색 기능
 	// 반환 타입 List<Dept>
 	// 매개변수 - Connection 객체 : Statement
@@ -47,7 +51,10 @@ public class DeptDao {
 
 			// 데이터를 Dept 객체로 생성 -> list에 저장
 			while (rs.next()) {
-				list.add(new Dept(rs.getInt(1), rs.getString(2), rs.getString(3)));
+				
+				Dept d = new Dept(rs.getInt(1), rs.getString(2), rs.getString(3));
+				
+				list.add(d);
 			}
 
 		} catch (SQLException e) {
@@ -55,8 +62,8 @@ public class DeptDao {
 			e.printStackTrace();
 
 		} finally {
-
-			if (rs != null) {
+			
+			if(rs != null) {
 				try {
 					rs.close();
 				} catch (SQLException e) {
@@ -64,7 +71,7 @@ public class DeptDao {
 					e.printStackTrace();
 				}
 			}
-
+			
 			if (stmt != null) {
 				try {
 					stmt.close();
@@ -94,14 +101,15 @@ public class DeptDao {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, dept.getDname());
 			pstmt.setString(2, dept.getLoc());
-
+			
 			result = pstmt.executeUpdate();
-
+			
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if (pstmt != null) {
+			if(pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
@@ -114,11 +122,12 @@ public class DeptDao {
 		return result;
 	}
 
+
 	// 3. DEPT 테이블의 데이터 수정 메소드
 	// 반영된 행의 개수 반환
 	// 사용자로부터 데이터를 받아서 처리 -> Dept 객체
 	int editDept(Connection conn, Dept dept) {
-
+		
 		int result = 0;
 
 		// 전달받은 Dept 객체의 데이터로 Dept 테이블에 저장 -> 결과 값을 반환
@@ -130,14 +139,15 @@ public class DeptDao {
 			pstmt.setString(1, dept.getDname());
 			pstmt.setString(2, dept.getLoc());
 			pstmt.setInt(3, dept.getDeptno());
-
+			
 			result = pstmt.executeUpdate();
-
+			
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if (pstmt != null) {
+			if(pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
@@ -149,41 +159,49 @@ public class DeptDao {
 
 		return result;
 	}
+
 
 	// 4. DEPT 테이블의 데이터를 삭제
 	// 삭제된 행의 개수를 반환
 	// 사용자로부터 deptno 받아서 처리
-
 	int deleteDept(Connection conn, int deptno) {
-
+		
 		int result = 0;
-
+		
 		// 데이터 베이스 처리 sql
 		PreparedStatement pstmt = null;
 		String sql = "delete from dept where deptno=?";
+		
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, deptno);
-
+			
 			result = pstmt.executeUpdate();
-
+			
+			
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if (pstmt != null) {
+			
+			if(pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
 			}
 		}
-
+		
+		
 		return result;
-
+		
 	}
+
+
+
+
 
 }
