c1dc9245a96882960191461e1a8fde7c8ec5619c
kimrumm <83346234+kimrumm@users.noreply.github.com>
2021-07-13 01:45:46 +0900

    .

--- Web/jquery/project/manager.js
+++ Web/jquery/project/manager.js
@@ -1,17 +1,3 @@
-
-
-// 회원의 정보 : 아이디, 비밀번호, 이름
-// Member -> 생성자 함수를 정의
-function Member(id, pw, name) {
-    this.userid = id;
-    this.pw = pw;
-    this.username = name;
-}
-// 객체가 가지는 메소드는 공통으로 사용 -> prototype
-Member.prototype.makeHtml = function () {
-    return '[id:' + this.userId + ' , pw: ' + this.pw + ' , name: ' + this.userName + ' ]'
-};
-
 //////////////////////////////////////////////////////////
 
 // 회원의 정보를 저장하는 배열
@@ -28,6 +14,10 @@ var members = []; // new Array()
 
 // 사용자 입력한 정보를 가지고 Member객체를 생성
 // submit  이벤트 연결
+
+// onload -> $(document).ready(fn());
+
+// memberReg 함수로 분리
 
 window.onload = function () {
 
